7e58b00a015e31b444fa29f473afb5f3fa37abc8
Xinjiang Lu <xinjiang.lu@gmail.com>
2013-06-01 15:41:05 -0400

    crud for student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,41 @@
 class StudentsController < ApplicationController
+  respond_to :json
+
   def index
   end
 
   def create
+    @student = Student.create(
+      :first_name => params[:first_name],
+      :last_name => params[:last_name],
+      :date_of_birth => params[:date_of_birth]
+    )
+
+    respond_with @student
   end
 
   def show
+    @student = Student.find(params[:id])
+
+    respond_with @student
   end
 
   def update
+    @student = Student.find(params[:id])
+
+    @student.update_attributes(
+      :first_name => params[:first_name],
+      :last_name => params[:last_name],
+      :date_of_birth => params[:date_of_birth]
+    )
+
+    respond_with @student
   end
 
   def destroy
+    @student = Student.find(params[:id])
+    @student.disable
+
+    respond_with @student
   end
 end
