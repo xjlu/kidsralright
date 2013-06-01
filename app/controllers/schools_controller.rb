ec181db62bd7191b257e562c77b87841250a1ad4
Xinjiang Lu <xinjiang.lu@gmail.com>
2013-06-01 15:33:29 -0400

    school controller methods

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,13 +1,33 @@
 class SchoolsController < ApplicationController
+  respond_to :json
+
   def index
   end
 
+  # params: :name, :address
   def create
+    @school  = School.create(
+      :name => params[:name],
+      :address => params[:address]
+    )
+
+    respond_with @school
   end
 
   def update
+    @school = School.find(params[:id])
+    @school.update_attributes(
+      :name => params[:name],
+      :address => params[:address]
+    )
+
+    respond_with @school
   end
 
   def destroy
+    @school = School.find(params[:id])
+    @school.destroy
+
+    respond_with @school
   end
 end
