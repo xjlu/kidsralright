3046816f061208ae1a5589b5a3b1608e3f325efa
Xinjiang Lu <xinjiang.lu@gmail.com>
2013-06-01 15:56:38 -0400

    routes and controller methods

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,13 +1,29 @@
 class ClassroomsController < ApplicationController
+  respond_to :json
+
   def index
+    @school = School.find(params[:id])
+    respond_with @school.classrooms
   end
 
   def create
+    @classroom = Classroom.create(
+      :name => params[:name])
+
+    respond_with @classroom
   end
 
   def update
+    @classroom = Classroom.find(params[:id])
+    @classroom.update_attributes(:name => params[:name])
+
+    respond_with @classroom
   end
 
   def destroy
+    @classroom = Classroom.find(params[:id])
+    @classroom.destroy
+
+    respond_with @classroom
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Kidsralright::Application.routes.draw do
   devise_for :users
 
   match '/', :to => "application#index"
+
+  resources :schools do
+    resources :classrooms
+    resources :students
+  end
+
+  resources :accounts
 end
