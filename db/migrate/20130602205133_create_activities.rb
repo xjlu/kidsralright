b236c2b886423cc750c0cce2cbb9e8ec5a26f538
Xinjiang Lu <xinjiang.lu@gmail.com>
2013-06-03 01:23:12 -0400

    I don't think it's right yet, but I am committing it for now

--- db/migrate/20130602205133_create_activities.rb
+++ db/migrate/20130602205133_create_activities.rb
@@ -2,13 +2,26 @@ class CreateActivities < ActiveRecord::Migration
   def change
     # Activity name such as Art, Creative Movement, Math/Science,
     # Music/Role Play/Social Play, Language Development, Computer
-    # The user is allowed to come up with any name
+    # The user is allowed to come up with any name, but we build a seed list in a yml file for now
     create_table :activities do |t|
-      t.references :class
-      t.string :name
+      t.references :classes
       t.date :date
+      t.string :name
+      t.text :description
 
       t.timestamps
     end
+
+    # lessons can use acts_as_taggable_on, and using 'lessons' as the context
+
+    create_table :lessons do |t|
+      t.string :name
+    end
+
+    create_table :daily_lessons do |t|
+      t.references :activitites
+      t.references :lessons
+    end
+
   end
 end
