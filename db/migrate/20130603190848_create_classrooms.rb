dc965ec0a1fa5cc1e82aad29f4db019d9d4c1b72
Xinjiang Lu <xinjiang.lu@gmail.com>
2013-06-03 15:15:18 -0400

    add accounts

--- db/migrate/20130603190848_create_classrooms.rb
+++ db/migrate/20130603190848_create_classrooms.rb
@@ -10,6 +10,11 @@ class CreateClassrooms < ActiveRecord::Migration
     create_table :schools do |t|
       t.string :name
       t.string :address
+      t.references :accounts
+    end
+
+    create_table :accounts do |t|
+      t.string :name
     end
   end
 end
