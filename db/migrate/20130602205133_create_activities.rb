4aa33e70ba7f50f8ef81ade5e82ea69a22efa45e
Xinjiang Lu <xinjiang.lu@gmail.com>
2013-06-03 14:51:10 -0400

    remove sql version of activity list

--- db/migrate/20130602205133_create_activities.rb
+++ db/migrate/20130602205133_create_activities.rb
@@ -3,32 +3,32 @@ class CreateActivities < ActiveRecord::Migration
     # Activity name such as Art, Creative Movement, Math/Science,
     # Music/Role Play/Social Play, Language Development, Computer
     # The user is allowed to come up with any name, but we build a seed list in a yml file for now
-    create_table :activities do |t|
-      t.references :classes
-      t.date :date
-      t.string :name
-      t.text :description
+    # create_table :activities do |t|
+    #   t.references :classes
+    #   t.date :date
+    #   t.string :name
+    #   t.text :description
 
-      t.timestamps
-    end
+    #   t.timestamps
+    # endc
 
-    create_table :daily_activities |t|
-      t.references :classes
-      t.references :activites
-      t.references :lessons
-      t.date :date
-    end
+    # create_table :daily_activities |t|
+    #   t.references :classes
+    #   t.references :activites
+    #   t.references :lessons
+    #   t.date :date
+    # end
 
-    # lessons can use acts_as_taggable_on, and using 'lessons' as the context
+    # # lessons can use acts_as_taggable_on, and using 'lessons' as the context
 
-    create_table :lessons do |t|
-      t.string :name
-    end
+    # create_table :lessons do |t|
+    #   t.string :name
+    # end
 
-    create_table :daily_lessons do |t|
-      t.references :activitites
-      t.references :lessons
-    end
+    # create_table :daily_lessons do |t|
+    #   t.references :activitites
+    #   t.references :lessons
+    # end
 
   end
 end
