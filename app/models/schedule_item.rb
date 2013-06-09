6859cffa3ded1a41d48c6b712758bed2b96c51b9
Xinjiang Lu <xinjiang.lu@gmail.com>
2013-06-09 10:11:08 -0400

    add a validation, maybe I should should focus on other things first

--- app/models/schedule_item.rb
+++ app/models/schedule_item.rb
@@ -1,4 +1,14 @@
 class ScheduleItem < ActiveRecord::Base
   belongs_to :schedule
   validates :schedule_id, :start_time, :end_time, :title, :presence => true
+
+  default_scope order(:start_time)
+
+  validate :start_time_earlier_than_end_time
+
+  def start_time_earlier_than_end_time
+    if start_time > end_time
+      errors.add(:start_time, "can't be later than end time")
+    end
+  end
 end
