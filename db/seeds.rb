1063b3ca3d86c11f3c3bfba5388aec29ac934b02
Xinjiang Lu <xinjiang.lu@gmail.com>
2013-09-08 17:52:04 -0700

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,27 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-account = Account.where(:name => "Briarcliff Day Care").first_or_create!
+accounts = []
+["Briarcliff Day Care", "Bright Horizons Family Solutions"].each do |name|
+  accounts << Account.where(:name => name).first_or_create!
+end
 
-school = School.where(:name => "Briarcliff center", :account => account).first_or_create!
-
+school = School.where(:name => "Briarcliff center", :account => accounts.first).first_or_create!
 
 # create classrooms
 [ "Infants-1", "Infants-2", "Toddlers-1",
   "Toddlers-2", "Preschool-1", "Preschool-2"
 ].each do |name|
   classroom = Classroom.where(:name => name, :school => school).first_or_create!
-  classroom.schedules.where(:name => "2013 #{classroom.name} Summer Schedule").first_or_create!
+  # classroom.schedules.where(:name => "2013 #{classroom.name} Summer Schedule").first_or_create!
+end
+
+# brighton school @ mission bay
+ucsf_school = School.where(
+  name: "The University Child Care Center at Mission Bay",
+  account: accounts[1]).first_or_create!
+%w(pelican dolphin whale).each do |name|
+  Classroom.where(:name => name, :school => school).first_or_create!
 end
 
 # create class schedule for "Infants-2"
