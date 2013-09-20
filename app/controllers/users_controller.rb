9a19132a63a2b65ce3c4292266bcd329d78eed93
Xinjiang Lu <xinjiang.lu@gmail.com>
2013-09-19 23:12:17 -0700

    some customization of devise user signin routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,12 @@ class UsersController < ApplicationController
     render :json => @users
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def update
+    @user = User.find(params[:id])
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Kidsralright::Application.routes.draw do
   devise_for :users
+  get "/signin", to: "devise/session#new"
+  get "/signout", to: "devise/session#destroy"
 
   root to: "application#home_page"
   match "/timeline", controller: "timeline", action: "index", via: [:get]
+
+  resources :users
 
   resources :schools do
     resources :classrooms
