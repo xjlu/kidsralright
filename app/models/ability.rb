ec85954f3626c80ce63984924a60134bc7a2a856
Xinjiang Lu <xinjiang.lu@gmail.com>
2013-09-21 15:07:59 -0700

    some draft ability defs

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,7 +1,10 @@
 class Ability
   include CanCan::Ability
+  attr_reader :user
 
   def initialize(user)
+    @user = user
+    build_permissions
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
@@ -11,12 +14,12 @@ class Ability
     #     can :read, :all
     #   end
     #
-    # The first argument to `can` is the action you are giving the user 
+    # The first argument to `can` is the action you are giving the user
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
     # here are :read, :create, :update and :destroy.
     #
-    # The second argument is the resource the user can perform the action on. 
+    # The second argument is the resource the user can perform the action on.
     # If you pass :all it will apply to every resource. Otherwise pass a Ruby
     # class of the resource.
     #
@@ -29,4 +32,44 @@ class Ability
     # See the wiki for details:
     # https://github.com/ryanb/cancan/wiki/Defining-Abilities
   end
+
+  def build_permissions
+    if is_director?
+      can :manage, :all
+    end
+
+    if is_teacher?
+      can [:read, :create], Post, classroom: {school_id: user.school_ids}
+    end
+  end
+
+  def user_roles
+    @user_roles ||= @user.roles
+  end
+
+  # define helper methods for role checking
+  def is_director?
+    user_roles.include?(director_role)
+  end
+
+  def is_teacher?
+    user_roles.include?(teacher_role)
+  end
+
+  def is_guardian?
+    user_roles.include?(guardian_role)
+  end
+
+  def director_role
+    @director_role ||= Role.find_by_name("director")
+  end
+
+  def teacher_role
+    @teacher_role ||= Role.find_by_name("teacher")
+  end
+
+  def guardian_role
+    @guardian_role ||= Role.find_by_name("guardian")
+  end
+
 end
